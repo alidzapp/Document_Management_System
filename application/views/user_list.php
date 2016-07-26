7612db79585b8ba2de1a8bbadc3500b98822c6af
ali.yassine <ali.yassine@sclgroup.cc>
2016-07-26 16:59:51 +0100

    fixes

--- application/views/user_list.php
+++ application/views/user_list.php
@@ -1,52 +1,90 @@
-
-
-
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="card">
-                            <div class="header">
-                                <h4 class="title">Table Header Goes here</h4>
-                                <p class="category">subtitle goes here as well</p>
-                            </div>
-                            <div class="content table-responsive table-full-width">
-                                <table class="table table-hover table-striped">
-                                    <thead>
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Salary</th>
-                                    <th>Country</th>
-                                    <th>City</th>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Dakota Rice</td>
-                                        <td>$36,738</td>
-                                        <td>Niger</td>
-                                        <td>Oud-Turnhout</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Minerva Hooper</td>
-                                        <td>$23,789</td>
-                                        <td>Curaçao</td>
-                                        <td>Sinaai-Waas</td>
-                                    </tr>
-
-                                    </tbody>
-                                </table>
-
-                            </div>
-                        </div>
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="header">
+                        <h4 class="title">Table Header Goes here</h4>
+                        <p class="category">subtitle goes here as well</p>
                     </div>
+                    <div class="content table-responsive table-full-width">
+                        <table class="table table-hover table-striped">
+                            <thead>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Salary</th>
+                            <th>Country</th>
+                            <th>City</th>
+                            <th>&nbsp;</th>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Dakota Rice</td>
+                                <td>$36,738</td>
+                                <td>Niger</td>
+                                <td>Oud-Turnhout</td>
+                                <td>
+                                    <button type="button" class="btn btn-primary">Primary</button>
 
+                                    <!-- Secondary, outline button -->
+                                    <button type="button" class="btn btn-secondary">Secondary</button>
 
+                                    <!-- Indicates a successful or positive action -->
+                                    <button type="button" class="btn btn-success">Success</button>
 
+                                    <!-- Contextual button for informational alert messages -->
+                                    <button type="button" class="btn btn-info">Info</button>
 
+                                    <!-- Indicates caution should be taken with this action -->
+                                    <button type="button" class="btn btn-warning">Warning</button>
+
+                                    <!-- Indicates a dangerous or potentially negative action -->
+                                    <button type="button" class="btn btn-danger">Danger</button>
+
+                                    <!-- Deemphasize a button by making it look like a link while maintaining button behavior -->
+                                    <button type="button" class="btn btn-link">Link</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Minerva Hooper</td>
+                                <td>$23,789</td>
+                                <td>Curaçao</td>
+                                <td>Sinaai-Waas</td>
+                                <td>
+                                    <button type="button" class="btn btn-primary">Primary</button>
+
+                                    <!-- Secondary, outline button -->
+                                    <button type="button" class="btn btn-secondary">Secondary</button>
+
+                                    <!-- Indicates a successful or positive action -->
+                                    <button type="button" class="btn btn-success">Success</button>
+
+                                    <!-- Contextual button for informational alert messages -->
+                                    <button type="button" class="btn btn-info">Info</button>
+
+                                    <!-- Indicates caution should be taken with this action -->
+                                    <button type="button" class="btn btn-warning">Warning</button>
+
+                                    <!-- Indicates a dangerous or potentially negative action -->
+                                    <button type="button" class="btn btn-danger">Danger</button>
+
+                                    <!-- Deemphasize a button by making it look like a link while maintaining button behavior -->
+                                    <button type="button" class="btn btn-link">Link</button>
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+
+                    </div>
                 </div>
             </div>
+
+
         </div>
+    </div>
+</div>
 
 
